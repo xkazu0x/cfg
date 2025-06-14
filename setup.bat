ee6cff3d024805e0b04b45a5c78eb786c86e8744
xkazu0x <kazuooficial@hotmail.com>
2025-06-14 18:50:00 -0300

    setup

--- setup.bat
+++ setup.bat
@@ -1,13 +1,26 @@
-:: @echo off
+@echo off
 setlocal enabledelayedexpansion
 cd /D "%~dp0"
 
-:: --- Emacs Setup ---------------------------------------------
-copy .\.emacs %APPDATA%\.emacs
+:: --- Unpack Arguments ----------------------------------------
+for %%a in (%*) do set "%%a=1"
+if not "%emacs%"=="1" if not "%wezterm%"=="1" (
+   set emacs=1
+   set wezterm=1
+)
 
-if not exist %APPDATA%\.emacs.d\themes mkdir %APPDATA%\.emacs.d\themes
-copy .\excalibur-theme.el %APPDATA%\.emacs.d\themes\excalibur-theme.el
-copy .\excalibur-metal-theme.el %APPDATA%\.emacs.d\themes\excalibur-metal-theme.el
+:: --- Emacs Setup ---------------------------------------------
+if "%emacs%"=="1" (
+   echo [emacs setup]
+   copy .\.emacs %APPDATA%\.emacs
+
+   if not exist %APPDATA%\.emacs.d\themes mkdir %APPDATA%\.emacs.d\themes
+   copy .\excalibur-theme.el %APPDATA%\.emacs.d\themes\excalibur-theme.el
+   copy .\excalibur-metal-theme.el %APPDATA%\.emacs.d\themes\excalibur-metal-theme.el
+)
 
 :: --- Wezterm Setup -------------------------------------------
-copy .\.wezterm.lua %USERPROFILE%\.wezterm.lua
+if "%wezterm%"=="1" (
+   echo [wezterm setup]
+   copy .\.wezterm.lua %USERPROFILE%\.wezterm.lua
+)
