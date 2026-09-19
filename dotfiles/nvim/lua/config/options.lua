23d51c3821edd93f592d06b4d25e58f2e894a59b
xkazu0x <kazuooficial@hotmail.com>
2026-09-19 18:04:15 -0300

    fix nvim c indentation

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -2,10 +2,20 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.wrap = true
-vim.opt.smartindent = true
+vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.showbreak = string.rep(" ", 2)
-vim.opt.linebreak = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = false
+vim.opt.autoindent = true
+vim.opt.cindent = false
+vim.opt.cinoptions = "l1,t0"
+
 vim.opt.cursorline = true
 vim.opt.scrolloff = 5
 vim.opt.updatetime = 50
@@ -15,10 +25,5 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.shadafile = "NONE"
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
 
 vim.opt.fillchars = {vert = " ", vertleft = " ", vertright = " ", verthoriz = " ", horiz = " ", horizup = " ", horizdown = " "}
